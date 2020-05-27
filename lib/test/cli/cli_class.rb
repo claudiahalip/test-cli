081a61123e5f9b673da476f1380905e197cc8065
claudia v <claudia4ro@yahoo.com>
2020-05-27 21:32:03 +0000

    6

--- lib/test/cli/cli_class.rb
+++ lib/test/cli/cli_class.rb
@@ -21,10 +21,34 @@ class CLI
     if input == "yes"
       puts "Here is a list with all the characters:"
       display_list_ch
+      character_choise
     else
       puts "Bye!Come back later."
     end 
     
+    
+  end 
+  
+  def character_choise
+    
+    puts "What character are you interesting in?"
+    puts "Intoduce a number for your choise."
+    index = gets.chomp.to_i - 1
+    
+    until index.between?(1, Character.all.length - 1)
+      puts "Sorry, this is an invalid choise"
+      index = gets.chomp.to_i - 1
+    end 
+      
+    
+    
+    ch_inst = Character.all[index]
+    
+    character_details
+    
+  end
+  
+  def character_details
     
   end 
   
